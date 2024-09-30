79134fc17bc0de03d9018cd5407fc852a2466ecc
Aridan Pantoja <70522543+aridanpantoja@users.noreply.github.com>
2024-09-30 21:04:30 +0000

    style: add a responsive banner for larger screens

--- emails/components/banner.tsx
+++ emails/components/banner.tsx
@@ -3,8 +3,8 @@ import React from "react"
 
 export const Banner = () => {
     return (
-        <Container style={{ maxWidth: "100%" }}>
-            <Img src="https://linkjr.com.br/images/banner.png" alt="Banner do Processo Seletivo" style={{ width: "100%" }} />
+        <Container style={{ maxWidth: "100%", backgroundColor: "#FA7317" }}>
+            <Img src="https://linkjr.com.br/images/banner.png" alt="Banner do Processo Seletivo" style={{ width: "100%", margin: "0px auto"  }} />
         </Container>
     )
 }
