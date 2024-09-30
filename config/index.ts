dc8031856c0aa29b995f86691386be51f3d36697
Aridan <aridanpantoja@gmail.com>
2024-09-30 11:53:35 -0300

    feat: add a configuration file to links

---
+++ config/index.ts
@@ -0,0 +1,5 @@
+export const config = {
+    assetsUrl: 'https://raw.githubusercontent.com/linkjrcastanhal/email-templates/refs/heads/main/assets',
+    email: 'linkjrcastanhal@gmail.com',
+    instagram: 'https://www.instagram.com/linkjr.tech/',
+}
